73546eeef717c004bb1f7b25e8e77adfff33a064
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2020-12-14 13:37:08 +0100

    adw

--- SKP Screenviewer/Form1.Designer.cs
+++ SKP Screenviewer/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.waitText = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.port2 = new WindowsFormsApp4.TextBoxExControl();
             this.conId = new WindowsFormsApp4.TextBoxExControl();
             this.port = new WindowsFormsApp4.TextBoxExControl();
@@ -51,9 +52,14 @@
             // 
             // connect
             // 
-            this.connect.Location = new System.Drawing.Point(10, 243);
+            this.connect.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.connect.FlatAppearance.BorderSize = 3;
+            this.connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect.ForeColor = System.Drawing.Color.Gainsboro;
+            this.connect.Location = new System.Drawing.Point(3, 362);
             this.connect.Name = "connect";
-            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.Size = new System.Drawing.Size(208, 30);
             this.connect.TabIndex = 0;
             this.connect.Text = "Connect";
             this.connect.UseVisualStyleBackColor = true;
@@ -61,9 +67,14 @@
             // 
             // share
             // 
-            this.share.Location = new System.Drawing.Point(91, 243);
+            this.share.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.share.FlatAppearance.BorderSize = 3;
+            this.share.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.share.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.share.ForeColor = System.Drawing.Color.Gainsboro;
+            this.share.Location = new System.Drawing.Point(3, 392);
             this.share.Name = "share";
-            this.share.Size = new System.Drawing.Size(93, 23);
+            this.share.Size = new System.Drawing.Size(208, 29);
             this.share.TabIndex = 1;
             this.share.Text = "Share Screen";
             this.share.UseVisualStyleBackColor = true;
@@ -72,29 +83,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 144);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(7, 151);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.Size = new System.Drawing.Size(129, 16);
             this.label1.TabIndex = 4;
             this.label1.Text = "Connection String";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gainsboro;
             this.label2.Location = new System.Drawing.Point(3, 78);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.Size = new System.Drawing.Size(94, 16);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Port";
+            this.label2.Text = "Port Number";
             // 
             // SidePanel
             // 
             this.SidePanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.SidePanel.BackColor = System.Drawing.Color.DarkGray;
+            this.SidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
             this.SidePanel.Controls.Add(this.listen);
             this.SidePanel.Controls.Add(this.port2);
             this.SidePanel.Controls.Add(this.label3);
@@ -104,18 +116,22 @@
             this.SidePanel.Controls.Add(this.share);
             this.SidePanel.Controls.Add(this.label2);
             this.SidePanel.Controls.Add(this.label1);
-            this.SidePanel.Location = new System.Drawing.Point(-3, 76);
+            this.SidePanel.Location = new System.Drawing.Point(-3, 69);
             this.SidePanel.Name = "SidePanel";
-            this.SidePanel.Size = new System.Drawing.Size(214, 423);
+            this.SidePanel.Size = new System.Drawing.Size(214, 424);
             this.SidePanel.TabIndex = 6;
             this.SidePanel.TabStop = false;
-            this.SidePanel.Text = "Menu";
             // 
             // listen
             // 
-            this.listen.Location = new System.Drawing.Point(50, 354);
+            this.listen.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listen.FlatAppearance.BorderSize = 3;
+            this.listen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.listen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listen.ForeColor = System.Drawing.Color.Gainsboro;
+            this.listen.Location = new System.Drawing.Point(3, 335);
             this.listen.Name = "listen";
-            this.listen.Size = new System.Drawing.Size(75, 23);
+            this.listen.Size = new System.Drawing.Size(208, 27);
             this.listen.TabIndex = 11;
             this.listen.Text = "Listen";
             this.listen.UseVisualStyleBackColor = true;
@@ -124,17 +140,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(3, 309);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(7, 236);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.Size = new System.Drawing.Size(94, 16);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Port";
+            this.label3.Text = "Port Number";
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Chocolate;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(120)))), ((int)(((byte)(115)))));
             this.panel1.Controls.Add(this.TeamViewer);
             this.panel1.Location = new System.Drawing.Point(-6, 0);
             this.panel1.Name = "panel1";
@@ -144,13 +160,13 @@
             // TeamViewer
             // 
             this.TeamViewer.AutoSize = true;
-            this.TeamViewer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TeamViewer.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.TeamViewer.Location = new System.Drawing.Point(25, 27);
+            this.TeamViewer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamViewer.ForeColor = System.Drawing.Color.Gainsboro;
+            this.TeamViewer.Location = new System.Drawing.Point(49, 25);
             this.TeamViewer.Name = "TeamViewer";
-            this.TeamViewer.Size = new System.Drawing.Size(159, 20);
+            this.TeamViewer.Size = new System.Drawing.Size(120, 24);
             this.TeamViewer.TabIndex = 0;
-            this.TeamViewer.Text = "SKP ScreenViewer";
+            this.TeamViewer.Text = "SKP Viewer";
             // 
             // waitText
             // 
@@ -158,12 +174,12 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.waitText.AutoSize = true;
-            this.waitText.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.waitText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.waitText.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.waitText.Location = new System.Drawing.Point(415, 199);
+            this.waitText.BackColor = System.Drawing.Color.Silver;
+            this.waitText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.waitText.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.waitText.Location = new System.Drawing.Point(396, 239);
             this.waitText.Name = "waitText";
-            this.waitText.Size = new System.Drawing.Size(343, 37);
+            this.waitText.Size = new System.Drawing.Size(365, 37);
             this.waitText.TabIndex = 8;
             this.waitText.Text = "Waiting for Connection";
             // 
@@ -173,34 +189,54 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(209, 0);
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(211, 76);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(721, 493);
+            this.pictureBox1.Size = new System.Drawing.Size(721, 417);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panel2.Location = new System.Drawing.Point(211, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(718, 76);
+            this.panel2.TabIndex = 10;
+            // 
             // port2
             // 
-            this.port2.Location = new System.Drawing.Point(41, 308);
+            this.port2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.port2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.port2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.port2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.port2.Location = new System.Drawing.Point(6, 255);
             this.port2.Name = "port2";
             this.port2.Placeholder = "";
-            this.port2.Size = new System.Drawing.Size(100, 20);
+            this.port2.Size = new System.Drawing.Size(198, 22);
             this.port2.TabIndex = 10;
             // 
             // conId
             // 
-            this.conId.Location = new System.Drawing.Point(6, 163);
+            this.conId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.conId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conId.ForeColor = System.Drawing.Color.Gainsboro;
+            this.conId.Location = new System.Drawing.Point(6, 170);
+            this.conId.Multiline = true;
             this.conId.Name = "conId";
             this.conId.Placeholder = "";
-            this.conId.Size = new System.Drawing.Size(174, 20);
+            this.conId.Size = new System.Drawing.Size(202, 37);
             this.conId.TabIndex = 7;
             // 
             // port
             // 
-            this.port.Location = new System.Drawing.Point(41, 77);
+            this.port.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.port.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.port.ForeColor = System.Drawing.Color.Gainsboro;
+            this.port.Location = new System.Drawing.Point(6, 97);
             this.port.Name = "port";
             this.port.Placeholder = "";
-            this.port.Size = new System.Drawing.Size(100, 20);
+            this.port.Size = new System.Drawing.Size(202, 22);
             this.port.TabIndex = 6;
             // 
             // SKPScreenViewer
@@ -210,6 +246,7 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(929, 492);
             this.ControlBox = false;
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.waitText);
             this.Controls.Add(this.SidePanel);
@@ -244,6 +281,7 @@
         private System.Windows.Forms.Button listen;
         private WindowsFormsApp4.TextBoxExControl port2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- SKP Screenviewer/Form1.cs
+++ SKP Screenviewer/Form1.cs
@@ -42,6 +42,7 @@ namespace SKP_Screenviewer
         {
 
             InitializeComponent();
+            
         }
 
 
